9b2e89c526d9ba8b6ab590557482e4d92ba0bce7
P Nikita <92731180+nikita8763@users.noreply.github.com>
2023-10-15 01:18:14 +0530

    updated profile page

--- client/src/Pages/Profile/Profile.jsx
+++ client/src/Pages/Profile/Profile.jsx
@@ -10,9 +10,7 @@ import "./profile.css"
 import profileimg from '../../assets/profile.jpg'
 import Female from '../../assets/Female.png'
 import Male from '../../assets/Male.png'
-import phone from '../../assets/phone.png'
 import location from '../../assets/location.png'
-import status from '../../assets/status.png'
 import date from '../../assets/date.png'
 import ButtonComponent from '../../utils/Button/Button';
 
@@ -43,31 +41,6 @@ const Profile = () => {
     return (
         <>
             {
-                // showspin ? <Spinner /> : <div className="container">
-                //     <Card className='card-profile shadow col-lg-6 mx-auto mt-4'>
-                //         <Card.Body>
-                //             <Row>
-                //                 <div className="col">
-                //                     <div className="card-profile-stats d-flex ">
-                //                         {/* <img src={`${BASE_URL}/uploads/${userprofile.profile}`} alt="" /> */}
-                //                         <img src={ profileimg}/>
-                //                     </div>
-                //                 </div>
-                //             </Row>
-
-                //             <div className='info'>
-                //                 <h3 className='username'>{userprofile.fname +" "+ userprofile.lname}</h3>
-                //                 <h4><img src={email}/>&nbsp;<span className='icon_heading'>Email</span>&nbsp;- <span className='details'>{userprofile.email}</span> </h4>
-                //                 <h5><img src={phone}/>&nbsp;<span className='icon_heading'>Mobile</span>&nbsp;- <span className='details'>{userprofile.mobile}</span> </h5>
-                //                 <h5><img src={man}/>&nbsp;<span className='icon_heading'>Gender</span>&nbsp;- <span className='details'>{userprofile.gender}</span> </h5>
-                //                 <h4><img src={location}/>&nbsp;<span className='icon_heading'>Location</span>&nbsp;- <span className='details'>{userprofile.location}</span> </h4>
-                //                 <h4><img src={status}/>&nbsp;<span className='icon_heading'>Status</span>&nbsp;- <span className='details'>{userprofile.status}</span> </h4>
-                //                 <h5><img src={date}/>&nbsp;<span className='icon_heading'>Date Created</span>&nbsp;- <span className='details'>{moment(userprofile.datecreated).format("DD-MM-YYYY")}</span> </h5>
-                //                 <h5><img src={date}/>&nbsp;<span className='icon_heading'>Date Updated</span>&nbsp;- <span className='details'>{userprofile.dateUpdated}</span> </h5>
-                //             </div>
-                //         </Card.Body>
-                //     </Card>
-                // </div>
                 showspin ? <Spinner /> : <div className="profileCardContainer">
                     <div className='profileCard'>
 
